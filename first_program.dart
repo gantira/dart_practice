1832e573cc3ae4a656028d89eced3fa9b5736f43
Angga Gantira <angga.gantira@gmail.com>
2022-08-24 21:21:48 +0700

    Variabel dan Tipe Data Bahasa Pemrograman Dart | Tutorial Dart Untuk Persiapan Belajar Flutter

--- first_program.dart
+++ first_program.dart
@@ -1,3 +1,16 @@
 void main(List<String> args) {
-  print('Hello, rifqi');
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print('nama $name');
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects);
+  print(image);
 }
