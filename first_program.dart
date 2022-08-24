64576716086799ea76bb5fb222c5b8be957c96da
Angga Gantira <angga.gantira@gmail.com>
2022-08-24 21:28:40 +0700

    Tipe Data Boolean Bahasa Pemrograman Dart | Tutorial Dart Untuk Persiapan Belajar Flutter

--- first_program.dart
+++ first_program.dart
@@ -1,16 +1,24 @@
 void main(List<String> args) {
+  //String
   var name = 'Voyager I';
+  //Integer
   var year = 1977;
+  //Double
   var antennaDiameter = 3.7;
+  //List
   var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  //Map
   var image = {
     'tags': ['saturn'],
     'url': '//path/to/saturn.jpg'
   };
+  //boolean
+  var isLoggedIn = false;
 
-  print('Nama: ${name}');
-  print(year);
-  print(antennaDiameter);
-  print('Planet pertama: ${flybyObjects[0]}');
-  print(image);
+  // print('Nama: ${name}');
+  // print(year);
+  // print(antennaDiameter);
+  // print('Planet pertama: ${flybyObjects[0]}');
+  // print(image);
+  print('Status login: $isLoggedIn');
 }
