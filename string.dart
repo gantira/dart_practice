593efb73cfd27d07806da1c47dc8a604f6d31d0c
Angga Gantira <angga.gantira@gmail.com>
2022-08-24 22:14:43 +0700

    String (Lanjutan) Bahasa Pemrograman Dart | Tutorial Dart Untuk Persiapan Belajar Flutter

--- string.dart
+++ string.dart
@@ -18,5 +18,37 @@ void main(List<String> args) {
   //mengubah menjadi list
   print(daftarHewan.split(', ')[1]);
 
+  //menampilkan substring
   print(nama.substring(6, 9));
+
+  //menampilkan panjang string
+  print(nama.length);
+
+  //menghilangkan spasi di depan dan di belakang
+  print(nama.trim());
+
+  //menghilangkan spasi di depan
+  print(nama.trimRight());
+
+  //menghilankan spasi di belakang
+  print(nama.trimLeft());
+
+  //mendapatkan nilai desimal ASCII
+  print(nama.codeUnitAt(1));
+
+  //menampilkan index karakter dalam string
+  print(nama.indexOf('i'));
+
+  //mengecek apakah diawali dengan string/karakter tertentu
+  print(nama.startsWith('Rifqi'));
+
+  //mengecek apakah diakhir dengan string/karakter tertentu
+  print(nama.endsWith('Hardianto'));
+
+  //mengecek apakah string tersebut kosong
+  var kosong = '';
+  print(kosong.isEmpty);
+
+  // cek apakah string tersebut tidak kosong
+  print(kosong.isNotEmpty);
 }
