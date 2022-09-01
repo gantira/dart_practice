3a0869d12a46fd86cf4aeaad176089c412286dbc
Angga Gantira <angga.gantira@gmail.com>
2022-09-01 11:46:57 +0700

    Operator Bahasa Pemrograman Dart | Tutorial Dart Untuk Persiapan Belajar Flutter

---
+++ operator.dart
@@ -0,0 +1,53 @@
+void main(List<String> args) {
+  var a = 10;
+  var b = 4;
+  var c = a + b;
+
+  // Operands -> representasi dari data (a & b)
+  // Operators -> sesuatu yang memutuskan bagaimana operands akan diproses (+)
+
+  // Arithmetic Operators
+  print('Aritmethic Operators');
+  // Penjumlahan
+  var penjumlahan = a + b;
+  // Pengurangan
+  var pengurangan = a - b;
+  // Perkalian
+  var perkalian = a * b;
+  // Pembagian
+  var pembagian = a / b;
+  // Modulo
+  var sisa = a % b;
+
+  print(penjumlahan);
+  print(pengurangan);
+  print(perkalian);
+  print(pembagian);
+  print(sisa);
+
+  // Equality & Relational Operators
+  print('Equality & Relational Operators');
+  // lebih dari
+  print(a > b);
+  // kurang dari
+  print(a < b);
+  // sama dengan
+  print(a == b);
+  // tidak sama dengan
+  print(a != b);
+  // lebih dari atau sama dengan
+  print(a >= b);
+  // kurang dari atau sama dengan
+  print(a <= b);
+
+  // Logical Operators
+  print('Logical Operators');
+  bool x = true;
+  bool y = false;
+  // && dan -> bernilai false apabila salah satu bernilai false
+  print(x && y);
+  // || OR -> bernilai true apabila salah satu bernilai true
+  print(x || y);
+  // ! NOT -> nliai menjadi berlawanan
+  print(!y);
+}
