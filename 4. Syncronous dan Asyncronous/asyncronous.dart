8888e1ebd3b80ef1971f2abda5ce89cb729a5d6b
Angga Gantira <angga.gantira@gmail.com>
2022-09-09 23:01:21 +0700

    Future, Async Await Bahasa Pemrograman Dart | Tutorial Dart Untuk Persiapan Belajar Flutter

--- 4. Syncronous dan Asyncronous/asyncronous.dart
+++ 4. Syncronous dan Asyncronous/asyncronous.dart
@@ -1,13 +1,18 @@
 import 'syncronous.dart';
 
-void main(List<String> args) {
-  fetchData();
-  print('menunggu data');
+Future<void> main(List<String> args) async {
+  print(await printData());
+  print('data telah selesai diproses');
 }
 
-Future<void> fetchData() {
+Future<String> printData() async {
+  var data = await fetchData();
+  return 'data: $data';
+}
+
+Future<String> fetchData() {
   return Future.delayed(
     Duration(seconds: 2),
-    () => print('fetch data'),
+    () => 'fetch data',
   );
 }
