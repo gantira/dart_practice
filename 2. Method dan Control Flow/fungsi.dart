cc315b5f53d27e7a6a587f580979b0e33f908d2f
Angga Gantira <angga.gantira@gmail.com>
2022-09-09 19:36:19 +0700

    Arrow Function Bahasa Pemrograman Dart | Tutorial Dart Untuk Persiapan Belajar Flutter

--- 2. Method dan Control Flow/fungsi.dart
+++ 2. Method dan Control Flow/fungsi.dart
@@ -4,19 +4,20 @@ void main(List<String> args) {
     //kode
     return nilai (sesuai tipe);
   }
+  tipe namaFungsi(parameter) => return nilai
   */
+
   String nama = 'Rifqi Eka Hardianto';
   perkenalan(nama);
 
   int sisi = 10;
   int volume = volumeKubus(sisi);
   print(volume);
+  print('phi ${nliaiPhi()}');
 }
 
-void perkenalan(String nama) {
-  print('Halo, nama saya $nama');
-}
+void perkenalan(String nama) => print('Halo, nama saya $nama');
 
-int volumeKubus(int sisi) {
-  return sisi * sisi * sisi;
-}
+int volumeKubus(int sisi) => sisi * sisi * sisi;
+
+double nliaiPhi() => 3.14;
